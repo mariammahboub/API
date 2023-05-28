d47d67047ff512b268b8109b98c16b0c70c4d8c2
mariam mahboub <mahboubmariam949@gmail.com>
2023-05-28 10:00:07 -0500

    Relationship created

--- app/Models/Model/Product.php
+++ app/Models/Model/Product.php
@@ -2,14 +2,21 @@
 
 namespace App\Models\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Model\Review;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
-    protected $table="products";
-    protected  $fillable=['name','details','price','stock','discount','created_at','updated_at'];
-    protected $hidden=['created_at','updated_at'];
-    public $timestamps=true;
+    
+    protected $table = "products";
+    protected $fillable = ['name', 'details', 'price', 'stock', 'discount', 'created_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at'];
+    public $timestamps = true;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -2,14 +2,21 @@
 
 namespace App\Models\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Model\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Review extends Model
 {
     use HasFactory;
-    protected $table="reviews";
-    protected  $fillable=['product_id','customer','review','star','created_at','updated_at'];
-    protected $hidden=['created_at','updated_at'];
-    public $timestamps=true;
+    
+    protected $table = "reviews";
+    protected $fillable = ['product_id', 'customer', 'review', 'star', 'created_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at'];
+    public $timestamps = true;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
