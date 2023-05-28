ece7b9e546f98b8adb4fae3def685874cda264f9
mariam mahboub <mahboubmariam949@gmail.com>
2023-05-28 08:14:39 -0500

    Migration created for product and review

--- database/migrations/2023_05_28_123125_create_products_table.php
+++ database/migrations/2023_05_28_123125_create_products_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('details');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
+           $table->timestamps();
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ProductController;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::apiResource('/product', 'ProductController');
 
