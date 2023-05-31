853a4425056d296cfb8f0c3fcb20051224fe7238
mariam mahboub <mahboubmariam949@gmail.com>
2023-05-31 09:51:17 -0500

    Create Token Api

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->char('api_token',100)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
